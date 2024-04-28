eb99d86337bcd232480acdcf82f39725839a4fa3
bipproduction <bip.production.js@gmail.com>
2024-04-28 19:38:17 +0800

    auto push

--- bin/app/list_project/index.js
+++ bin/app/list_project/index.js
@@ -7,15 +7,19 @@ module.exports = async function (req, res) {
     let list = (await fs.promises.readdir(path.join(root_path, "../"))).filter(x => x !== ".DS_Store")
 
     for (let l of list) {
-        const name = l
-        const type = JSON.parse(fs.readFileSync(path.join(root_path, "../", l, "package.json"), "utf-8")).dependencies.express
+        try {
+            const name = l
+            const type = JSON.parse(await fs.promises.readFile(path.join(root_path, "../", l, "package.json"), "utf-8")).dependencies.express ? "express" : "nextjs"
+            console.log(name, type)
+            const data = {
+                name,
+                type
+            }
 
-        const data = {
-            name,
-            type
+            list_data.push(data)
+        } catch (error) {
+            console.log(error)
         }
-
-        list_data.push(data)
 
     }
 
