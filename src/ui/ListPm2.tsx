1a6f8b1228e1c9c82f164990eca26e2a7865384b
bipproduction <bip.production.js@gmail.com>
2024-04-28 20:34:28 +0800

    auto push

--- src/ui/ListPm2.tsx
+++ src/ui/ListPm2.tsx
@@ -23,7 +23,7 @@ export function ListPm2() {
 
                 <Box p={"md"}>
                     <Skeleton visible={listPm2 === null}  h={200}>
-                        <Flex wrap={"wrap"}>
+                        <Flex wrap={"wrap"} gap={"md"}>
                             {listPm2?.map((x, i) => (
                                 <Paper shadow="sm" key={i} p={"md"} bg={x.status === "online" ? "green" : "red"} c={"white"}>
                                     <Stack key={i} gap={0}>
