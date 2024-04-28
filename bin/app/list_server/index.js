818f5fed16986fdf9b1bbf4a1a5d89093dd43b30
bipproduction <bip.production.js@gmail.com>
2024-04-28 19:43:50 +0800

    auto push

--- bin/app/list_server/index.js
+++ bin/app/list_server/index.js
@@ -4,11 +4,21 @@ const _ = require("lodash")
 module.exports = async function (req, res) {
     try {
         let list = await fs.promises.readdir("/etc/nginx/sites-enabled")
-        list = list.filter(x => x !== "default").map(async (x) => ({
-            name: x.split("_")[0],
-            port: Number(x.split("_")[1].split(".")[0]),
-            host: await fs.promises.readFile(`/etc/nginx/sites-enabled/${x}`, "utf-8")
-        }))
+        // list = list.filter(x => x !== "default").map(async (x) => ({
+        //     name: x.split("_")[0],
+        //     port: Number(x.split("_")[1].split(".")[0]),
+        //     host: await fs.promises.readFile(`/etc/nginx/sites-enabled/${x}`, "utf-8")
+        // }))
+
+        for (let l of list) {
+            l.name = l.split("_")[0]
+            l.port = Number(l.split("_")[1])
+            try {
+                l.host = await fs.promises.readFile(`/etc/nginx/sites-enabled/${l}`, "utf-8")
+            } catch (error) {
+                console.log(error)
+            }
+        }
 
         list = _.orderBy(list, ["port"], ["asc"])
         return res.json(list)
