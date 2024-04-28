6cdd44a0c6b4549d53fc6e95afc2c597fb349c6d
bipproduction <bip.production.js@gmail.com>
2024-04-28 19:19:46 +0800

    auto push

--- bin/app/list_server/index.js
+++ bin/app/list_server/index.js
@@ -1,8 +1,15 @@
 const fs = require("fs")
+const _ = require("lodash")
 
 module.exports = async function (req, res) {
     try {
-        const list = await fs.promises.readdir("/etc/nginx/sites-enabled")
+        let list = await fs.promises.readdir("/etc/nginx/sites-enabled")
+        list = list.filter(x => x !== "default").map(x => ({
+            name: x.split("_")[0],
+            port: Number(x.split("_")[1].split(".")[0]),
+        }))
+
+        list = _.orderBy(list, ["port"], ["asc"])
         return res.json(list)
     } catch (error) {
         return res.json([])
