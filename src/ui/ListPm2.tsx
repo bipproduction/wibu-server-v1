8a77056e15723eae43a4efa481fae8c8766a50aa
bipproduction <bip.production.js@gmail.com>
2024-04-28 20:29:07 +0800

    auto push

--- src/ui/ListPm2.tsx
+++ src/ui/ListPm2.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import { LoadingOverlay, Skeleton, Stack } from "@mantine/core";
+import { ActionIcon, Box, Divider, Flex, LoadingOverlay, Paper, Skeleton, Stack, Text } from "@mantine/core";
 import { useShallowEffect } from "@mantine/hooks";
 import { useState } from "react";
-import { Loader } from "./component/Loader";
+import { MdLogoDev, MdRestartAlt, MdStop } from 'react-icons/md'
+import _ from "lodash";
 
 export function ListPm2() {
     const [listPm2, setlistPm2] = useState<any[] | null>(null)
@@ -20,11 +21,49 @@ export function ListPm2() {
             <h1>ListPm2</h1>
             <Stack pos={"relative"}>
 
-                <Skeleton visible={listPm2 === null} >
-                    <pre>
-                        {JSON.stringify(listPm2, null, 2)}
-                    </pre>
-                </Skeleton>
+                <Box p={"md"}>
+                    <Skeleton visible={listPm2 === null}  h={200}>
+                        <Flex wrap={"wrap"}>
+                            {listPm2?.map((x, i) => (
+                                <Paper shadow="sm" key={i} p={"md"} bg={x.status === "online" ? "green" : "red"} c={"white"}>
+                                    <Stack key={i} gap={0}>
+                                        <Flex>
+                                            <Text w={100}>Id  </Text>
+                                            <Text >: {x.id}</Text>
+                                        </Flex>
+                                        <Flex>
+                                            <Text w={100}>Name </Text>
+                                            <Text >: {x.name}</Text>
+                                        </Flex>
+                                        <Flex>
+                                            <Text w={100}>Port </Text>
+                                            <Text >: {x.port}</Text>
+                                        </Flex>
+                                        <Flex>
+                                            <Text w={100}>Status </Text>
+                                            <Text >: {x.status}</Text>
+                                        </Flex>
+                                        <Divider mt={"md"} />
+                                        <Flex gap={"sm"} pt={"md"} >
+                                            <ActionIcon>
+                                                <MdRestartAlt />
+                                            </ActionIcon>
+                                            <ActionIcon>
+                                                <MdStop />
+                                            </ActionIcon>
+                                            <ActionIcon>
+                                                <MdLogoDev />
+                                            </ActionIcon>
+                                        </Flex>
+                                    </Stack>
+                                </Paper>
+                            ))}
+                        </Flex>
+                        {/* <pre>
+                            {JSON.stringify(listPm2, null, 2)}
+                        </pre> */}
+                    </Skeleton>
+                </Box>
             </Stack>
         </div>
     );
